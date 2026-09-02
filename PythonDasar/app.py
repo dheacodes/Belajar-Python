816424a6252d69a94f00ced47cdb036450565ae0
dhfebriant <dhfebriant@gmail.com>
2026-09-02 10:55:23 +0700

    Hari 1: Type Data, Format, Print & Latihan

---
+++ PythonDasar/app.py
@@ -0,0 +1,61 @@
+print("Hello "*3)
+#aa 
+""""
+PEMBELAJARAN PYTHON DASAR, PRINT & VARIABEL
+"""
+user = "Dhea"
+univ = 'UPI'
+print(user)
+
+'''
+integer = bilangan bulat. float = desimal, string = kata
+'''
+a = int(3)
+b = float(3)
+c = str(3)
+
+print(a)
+print(b)
+print(c)
+
+'''
+banyak nilai/variabel to bbrp variabel + banyak print
+'''
+A, B, C = 'apel', 'belimbing', 'ceri'
+D = E = F = G = "Makan";
+print(A)
+print(C)
+print("Halo ", user, "Di Univ ", univ)
+
+x, y = 2, 6
+print(str(x+y) + "5" * 4)
+
+'''
+Input & Output
+'''
+a = 90
+b = 80
+nama = input('Masukkan Nama Anda : ')
+print(f"Selamat datang, {nama}")
+print('Nilai A adalah ', a)
+umur = input('Masukkan umur anda')
+print(type(umur))
+
+'''
+fungsi numeric absolute, min, max, pow  , slicing
+'''
+pagi = "pagii"
+print (pagi[1:2])
+'''bisa [indexstart, indexfinish'''
+'''
+strip() = hapus apsi
+replace() = ganti spasi
+spit() = ganti string jadi list'''
+
+'''
+Escape Chr
+\' = single quotes
+\\backslash
+\n  = enter
+'''
+
---
+++ PythonDasar/latihan1.py
@@ -0,0 +1,4 @@
+nama = input('Masukkan nama Anda ')
+tahunlahir = input('Masukkan Tahun Lahir ')
+umur = 2026 - int(tahunlahir)
+print(f'Selamat datang', {nama}, '!', 'Umur Anda sekarang adalah ', {umur})
